544dd869292a5df52a6955bc15c7eb4821dbe11a
comolove <jungjinwoo41@gmail.com>
2023-06-11 15:45:35 +0900

    integrate springboot API

---
+++ app/app.tsx
@@ -0,0 +1,13 @@
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
+
+export default function MyApp({ Component, pageProps }) {
+  const [queryClient] = React.useState(() => new QueryClient())
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  )
+}
